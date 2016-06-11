cad9c9207765dc8ba2bc1a08e4c298fb6ee71c78
Tony Chen <tony.chen@8x8.com>
2016-06-11 16:19:10 -0700

    poc-firebase

--- src/main/java/com/clientsbox/data/repository/UserRepository.java
+++ src/main/java/com/clientsbox/data/repository/UserRepository.java
@@ -1,5 +1,82 @@
 package com.clientsbox.data.repository;
 
-public class UserRepository {
-    
+import com.clientsbox.core.model.User;
+import com.clientsbox.core.model.UserSession;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository implements IUserRepository {
+
+    @Override
+    public List<User> getAllUsers(UserSession mUserSession) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public User getUserById(String id, UserSession mUserSession) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createUser(User mUser, UserSession mUserSession) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateUser(User mUser, UserSession mUserSession) {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        String Url = classLoader.getResource("serviceAccountCredentials.json").getPath();
+
+        try {
+            FirebaseOptions options;
+            options = new FirebaseOptions.Builder()
+                    .setServiceAccount(new FileInputStream(Url))
+                    .setDatabaseUrl("https://zpay-4b5d6.firebaseio.com/")
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(UserRepository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("oAuthRecord/");
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot ds) {
+                Object document = ds.getValue();
+                System.out.println(document);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError de) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+        });
+        
+        
+
+    }
+
+    @Override
+    public void deleteUser(User mUser, UserSession mUserSession) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
--- src/main/java/com/clientsbox/logic/services/UserService.java
+++ src/main/java/com/clientsbox/logic/services/UserService.java
@@ -2,11 +2,16 @@ package com.clientsbox.logic.services;
 
 import com.clientsbox.core.model.User;
 import com.clientsbox.core.model.UserSession;
+import com.clientsbox.data.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements IUserService{
 
+    @Autowired
+    IUserRepository _userRepository;
+    
     @Override
     public UserSession getUserSessionByUsernamePassword(User mUser) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -14,7 +19,9 @@ public class UserService implements IUserService{
 
     @Override
     public UserSession registerUser(User mUser) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        UserSession mUserSession = new UserSession();
+        _userRepository.updateUser(mUser, mUserSession);
+        return mUserSession;
     }
 
     @Override
